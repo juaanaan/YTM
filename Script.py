97c3b2257af6f6646eef6214dff6ee00d6e90026
juaanaan <144701220+juaanaan@users.noreply.github.com>
2024-03-13 18:49:00 +0100

    Falta acabar distintos casos en los que la reserva se cancela, primero comprobar si se reserva y enseguida se cancela, si va bien.

--- Script.py
+++ Script.py
@@ -2,12 +2,36 @@ import requests
 import re
 import time
 import json
+import sys
 from cryptography.fernet import Fernet
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from datetime import datetime, timedelta
 from urllib.parse import quote_plus
 
+def writeCSC(service_sheets, spreadsheet_id, index):
+    service_sheets.spreadsheets().values().update(spreadsheetId=spreadsheet_id,
+                                range='Hoja 1!G' + str((index + 2)),
+                                valueInputOption='USER_ENTERED',
+                                body={'values': [['CSC']]}
+                            ).execute()  
+
+def writeCCC(service_sheets, spreadsheet_id, index):
+    service_sheets.spreadsheets().values().update(
+                            spreadsheetId=spreadsheet_id,
+                            range='Hoja 1!G' + str((index + 2)),
+                            valueInputOption='USER_ENTERED',
+                            body={'values': [['CCC']]}
+                        ).execute()  
+
+def writeHash(service_sheets, spreadsheet_id, index, existingHash):
+    service_sheets.spreadsheets().values().update(
+                            spreadsheetId=spreadsheet_id,
+                            range='Hoja 1!N' + str((index + 2)),
+                            valueInputOption='USER_ENTERED',
+                            body={'values': [[existingHash]]}
+                        ).execute()
+    
 def cargar_clave():
     with open('/Users/Juaanaan_/Desktop/Yourtaximallorca/key.key', 'rb') as f:
         return f.read()
@@ -36,7 +60,7 @@ def iso8601_to_dd_mm_yyyy_hh_mm_ss(iso8601_string):
     except ValueError:
         return "Error: Formato de fecha y hora no válido"
 
-def generate_event_summary(code, flight_number, pickup_address, dropoff_address, pickup_time, client_name, email, phone, passengerCount, luggageCount, driverCode, pickup_maps_link, addOns):
+def generate_event_summary(code, flight_number, pickup_address, dropoff_address, pickup_time, client_name, email, phone, passengerCount, luggageCount, driverCode, pickup_maps_link, addOns, comments):
     """Función para generar el resumen del evento."""
     pickup_time = iso8601_to_dd_mm_yyyy_hh_mm_ss(pickup_time)
     summary = f"*Código de Reserva:* {code}\n\n"
@@ -50,26 +74,10 @@ def generate_event_summary(code, flight_number, pickup_address, dropoff_address,
     summary += f"*Teléfono Cliente:* {phone}\n\n"
     summary += f"*Número de Pasajeros:* {passengerCount}\n\n"
     summary += f"*Número de Maletas:* {int(luggageCount)}\n\n"
-    if addOns:
-        summary += "*Extras:*\n"
-        for addon in addOns:
-            summary += f"- {addon}\n"
-    summary += f"\n*Ruta Google Maps:* {pickup_maps_link}\n"
-    return summary
+    
+    if comments:
+        summary += f"*Comentarios:* {comments}\n\n"
 
-def generate_event_summary(code, flight_number, pickup_address, dropoff_address, pickup_time, client_name, email, phone, passengerCount, luggageCount, driverCode, pickup_maps_link, addOns):
-    pickup_time = iso8601_to_dd_mm_yyyy_hh_mm_ss(pickup_time)
-    summary = f"*Código de Reserva:* {code}\n\n"
-    summary += f"*Link conductor:* https://staging.transferz.taxi/{driverCode}\n\n"
-    summary += f"*Número de Vuelo:* {flight_number}\n\n"
-    summary += f"*Dirección de Recogida:* {pickup_address}\n\n"
-    summary += f"*Dirección de Destino:* {dropoff_address}\n\n"
-    summary += f"*Fecha de Recogida:* {pickup_time}\n\n"
-    summary += f"*Cliente:* {client_name}\n\n"
-    summary += f"*Email Cliente:* {email}\n\n"
-    summary += f"*Teléfono Cliente:* {phone}\n\n"
-    summary += f"*Número de Pasajeros:* {passengerCount}\n\n"
-    summary += f"*Número de Maletas:* {int(luggageCount)}\n\n"
     if addOns:
         summary += "*Extras:*\n"
         for addon in addOns:
@@ -103,7 +111,7 @@ def generate_google_maps_link(origin, destination):
     return f"https://www.google.com/maps/dir/?api=1&origin=current+location&destination={destination_encoded}&waypoints={origin_encoded}&travelmode=driving"
 
 
-def create_event(service, calendar_id, code, flight_number, pickup_address, dropoff_address, pickup_time, client_name, email, phone, passengerCount, luggageCount, pickup_maps_link, price, whatsapp_link, color_id, addOns):
+def create_event(service, calendar_id, code, flight_number, pickup_address, dropoff_address, pickup_time, client_name, email, phone, passengerCount, luggageCount, pickup_maps_link, price, whatsapp_link, color_id, addOns, comments):
     """Función para crear un evento en Google Calendar."""
     # Creación de la hora de finalización para evento en Google Calendar (+1 hora)
     pickup_time_dt = datetime.fromisoformat(pickup_time)
@@ -126,12 +134,19 @@ def create_event(service, calendar_id, code, flight_number, pickup_address, drop
 <b>Número de Pasajeros:</b> {passengerCount}<br>
 <b>Número de Maletas:</b> {int(luggageCount)}<br>
 """
+
     if addOns:
         event_description += "<b>Extras:</b>\n"
         event_description += "\n".join([f"- {addon}" for addon in addOns])
+    
+    # Añadir sección de comentarios solo si hay comentarios
+    if comments:
+        event_description += f"<br><b>Comentarios:</b> {comments}"
 
     # Agregar espaciado entre los elementos del evento
     event_description = event_description.replace("<br>", "<br><br>")
+
+    
 
     # Creación del objeto de evento
     event = {
@@ -142,7 +157,7 @@ def create_event(service, calendar_id, code, flight_number, pickup_address, drop
             'timeZone': 'Europe/Madrid',
         },
         'end': {
-            'dateTime': pickup_time_f,  
+            'dateTime': pickup_time_f,
             'timeZone': 'Europe/Madrid',
         },
         'colorId': color_id  # Especificar el color deseado
@@ -151,8 +166,8 @@ def create_event(service, calendar_id, code, flight_number, pickup_address, drop
     # Inserción del evento en Google Calendar
     event = service.events().insert(calendarId=calendar_id, body=event).execute()
     print('Evento creado: %s' % (event.get('htmlLink')))
+    sys.stdout.flush()
 
-# El resto del código se mantiene igual.
 
 
 def write_to_google_sheets(service, spreadsheet_id, data):
@@ -163,7 +178,7 @@ def write_to_google_sheets(service, spreadsheet_id, data):
     values = []
     for row in data:
         # Extraemos los datos de la fila
-        code, pickup_time, entry_or_exit, pickup_time_hour, pickup_address, dropoff_address, price = row
+        code, pickup_time, entry_or_exit, pickup_time_hour, pickup_address, dropoff_address, price, hash, status = row
 
         if pickup_address == "Aeropuerto":
             non_airport_address = dropoff_address
@@ -179,11 +194,23 @@ def write_to_google_sheets(service, spreadsheet_id, data):
             # Extraer la parte después del código postal
             extracted_address = match.group(1).strip()
             print("Parte después del código postal:", extracted_address)
-            # Agregamos los datos a la lista de valores
-            values.append([code, pickup_time, entry_or_exit, pickup_time_hour, extracted_address, price])
+            sys.stdout.flush()
+            
+            # Comprobamos si la reserva ha sido cancelada
+            if status == "CANCELLED_FREE":
+                status = "CSC"
+                values.append([code, pickup_time, entry_or_exit, pickup_time_hour, extracted_address, price, status, "", "", "", "", "", "", hash])
+            elif status == "CANCELLED_WITH_COSTS":
+                    status = "CCC"
+                    values.append([code, pickup_time, entry_or_exit, pickup_time_hour, extracted_address, price, status, "", "", "", "", "", "", hash])
+            else:
+                values.append([code, pickup_time, entry_or_exit, pickup_time_hour, extracted_address, price, "", "", "", "", "", "", "", hash])
+                
         else:
             print("No se encontró un código postal que comience con '07' o la dirección no sigue el formato esperado.")
-            values.append([code, pickup_time, entry_or_exit, pickup_time_hour, non_airport_address, price])
+            sys.stdout.flush()
+            # Si no se encuentra el patrón, insertamos el hash en la columna N y dejamos las otras columnas vacías
+            values.append([code, pickup_time, entry_or_exit, pickup_time_hour, non_airport_address, price, "", "", "", "", "", "", "", hash])
             
     body = {
         'values': values
@@ -198,6 +225,7 @@ def write_to_google_sheets(service, spreadsheet_id, data):
             body=body
         ).execute()
         print('Datos insertados correctamente en Google Sheets.')
+        sys.stdout.flush()
 
         # Obtener la cantidad de filas y columnas con datos
         sheet_metadata = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
@@ -231,6 +259,9 @@ def write_to_google_sheets(service, spreadsheet_id, data):
         ).execute()
     except Exception as e:
         print('Error al insertar datos en Google Sheets:', e)
+        sys.stdout.flush()
+
+
 
 def main():
     """
@@ -238,7 +269,7 @@ def main():
     """
     # Credenciales del servicio de Google Calendar
     credentials = service_account.Credentials.from_service_account_file(
-        '/Users/juaanaan_/Desktop/Yourtaximallorca/ytm-api-414822-2da39b0051e3.json',
+        '//Users/Juaanaan_/Desktop/Yourtaximallorca/ytm-api-414822-2da39b0051e3.json',
         scopes=['https://www.googleapis.com/auth/calendar']
     )
 
@@ -247,7 +278,7 @@ def main():
 
     # Credenciales del servicio de Google Sheets
     credentials_sheets = service_account.Credentials.from_service_account_file(
-        '/Users/juaanaan_/Desktop/Yourtaximallorca/ytm-api-414822-2da39b0051e3.json',
+        '/Users/Juaanaan_/Desktop/Yourtaximallorca/ytm-api-414822-2da39b0051e3.json',
         scopes=['https://www.googleapis.com/auth/spreadsheets']
     )
 
@@ -261,6 +292,7 @@ def main():
     current_time_minus_one_hour = datetime.now() - timedelta(hours=1, minutes=20)
     formatted_time = current_time_minus_one_hour.strftime('%Y-%m-%dT%H:%M:%S')
     print(formatted_time)
+    sys.stdout.flush()
     
     clave = cargar_clave()
 
@@ -272,7 +304,7 @@ def main():
 
     api_key = config["API_KEY"]
     # Construir la URL de la consulta con la hora actual menos una hora y diez minutos
-    url = f"https://warpdrive.staging.transferz.com/transfercompanies/journeys?excludedStatuses=COMPLETED&excludedStatuses=CANCELLED_FREE&excludedStatuses=CANCELLED_WITH_COSTS&excludedStatuses=BOOKER_CANCELLED&excludedStatuses=SUPPLIER_CANCELLED&createdAfter={formatted_time}&page=0&size=20"
+    url = f"https://warpdrive.staging.transferz.com/transfercompanies/journeys?excludedStatuses=COMPLETED&excludedStatuses=PENDING&updatedAfter={formatted_time}&page=0&size=20"
 
     headers = {
         "accept": "application/json",
@@ -280,15 +312,13 @@ def main():
     }
 
     # Obtener datos de la primera columna de la hoja de cálculo para verificar códigos de reserva existentes
-    range_name = 'Hoja 1!A2:A'  # Ajusta el rango según tu hoja de cálculo
-    result = service_sheets.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
+    result = service_sheets.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range='Hoja 1!A2:A').execute()
     values = result.get('values', [])
     
     # Extraer los códigos de reserva existentes de la hoja de cálculo
     existing_codes = [row[0] for row in values if row]
 
-    # Realizar la solicitud HTTP para obtener datos de transferencias
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, timeout=10)  # Tiempo de espera de 10 segundos
 
     if response.status_code == 200:
         data = response.json()
@@ -296,11 +326,33 @@ def main():
         if results:
             for result in results: # Obtención de datos para transfer
                 code = result.get("code")
+                hash = result.get("hash")
+                status = result.get("status")
 
                 # Verificar si el código de reserva ya existe en la hoja de cálculo
                 if code in existing_codes:
-                    print(f"El servicio con el código de reserva {code} ya está en la hoja de cálculo. Descartando...")
-                    continue
+                    # Comprobamos si el hash ha cambiado
+                    index = existing_codes.index(code)
+                    existingHash = service_sheets.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range='Hoja 1!N' + str((index + 2))).execute()
+                    
+                    if existingHash == hash:
+                        print(f"El servicio con el código de reserva {code} ya está en la hoja de cálculo. Descartando...")
+                        sys.stdout.flush()
+                        continue
+                    
+                    # En caso de que no coincidan los hash, querrá decir que ha habido una actualización en la reserva
+                    updated = True
+
+                    # Comprobamos si la reserva ha sido cancelada
+                    
+                    if status == "CANCELLED_FREE":
+                        writeCSC(service_sheets, spreadsheet_id, index)    
+                        writeHash(service_sheets, spreadsheet_id, index, existingHash)             
+                        continue
+                    elif status == "CANCELLED_WITH_COSTS":          
+                        writeCCC(service_sheets, spreadsheet_id, index)
+                        writeHash(service_sheets, spreadsheet_id, index, existingHash)
+                        continue
                 
                 flight_number = result.get("travellerInfo", {}).get("flightNumber")
                 pickup_address = result.get("pickup", {}).get("resolvedAddress")
@@ -315,11 +367,11 @@ def main():
                 phone = result.get("travellerInfo", {}).get("phone")
                 passengerCount = result.get("travellerInfo", {}).get("passengerCount")
                 luggageCount = result.get("travellerInfo", {}).get("luggageCount")
+                comments = result.get("travellerInfo", {}).get("driverComments")
                 price  = result.get("fareSummary",{}).get("includingVat")
                 driverCode = result.get("driverCode",{})
                 vehicleCategory = result.get("vehicleCategory",{})
-                addOns = result.get("addOns", [])
-
+                addOns = result.get("addOns", [])               
                 # Creación ruta Google Maps
 
                 pickup_maps_link = generate_google_maps_link(pickup_address, dropoff_address)
@@ -328,14 +380,15 @@ def main():
 
                 pickup_address, dropoff_address, color_id = airport(pickup_address, dropoff_address, vehicleCategory)
 
-                # Creación de Whatsapp
+                if status != "CANCELLED_FREE" or status != "CANCELLED_WITH_COSTS":
+                    # Creación de Whatsapp
 
-                event_summary = generate_event_summary(code, flight_number, pickup_address, dropoff_address, result.get("pickupTime", {}).get("localTime"), client_name, email, phone, passengerCount, luggageCount, driverCode, pickup_maps_link, addOns)
-                whatsapp_link = generate_whatsapp_link(event_summary)
+                    event_summary = generate_event_summary(code, flight_number, pickup_address, dropoff_address, result.get("pickupTime", {}).get("localTime"), client_name, email, phone, passengerCount, luggageCount, driverCode, pickup_maps_link, addOns, comments)
+                    whatsapp_link = generate_whatsapp_link(event_summary)
 
-                # Creación del nuevo evento
+                    # Creación del nuevo evento
 
-                create_event(service, 'juanangonzcomas@gmail.com', code, flight_number, pickup_address, dropoff_address, result.get("pickupTime", {}).get("localTime"), client_name, email, phone, passengerCount, luggageCount, pickup_maps_link, price, whatsapp_link, color_id, addOns)
+                    create_event(service, 'juanangonzcomas@gmail.com', code, flight_number, pickup_address, dropoff_address, result.get("pickupTime", {}).get("localTime"), client_name, email, phone, passengerCount, luggageCount, pickup_maps_link, price, whatsapp_link, color_id, addOns, comments)
 
                 rows_to_append = []
                 
@@ -346,15 +399,23 @@ def main():
                 pickup_day, pickup_hour = pickup_time.split()
 
                 # Agregamos los datos a rows_to_append
-                rows_to_append.append([code, pickup_day, entry_or_exit, pickup_hour, pickup_address, dropoff_address, price])
+                
+                rows_to_append.append([code, pickup_day, entry_or_exit, pickup_hour, pickup_address, dropoff_address, price, hash, status])
 
                 write_to_google_sheets(service_sheets, spreadsheet_id, rows_to_append)
 
     else:
         print("La solicitud no fue exitosa. Código de estado:", response.status_code)
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     while True:
-        main()
-        # Esperar 5 minutos antes de la próxima ejecución
-        time.sleep(300)  # 300 segundos = 5 minutos
+        try:
+            main()
+            # Esperar 5 minutos antes de la próxima ejecución
+            time.sleep(300)  # 300 segundos = 5 minutos
+        except Exception as e :
+            print("Error al ejecutar Script:")
+            print("")
+            print(e)
+            continue
